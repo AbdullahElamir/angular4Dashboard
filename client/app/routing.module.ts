7707041858b9dc55e7a2190d38be1ae3d4d5bcf7
abdullah <abdullah.el_ameer@yahoo.com>
2017-07-04 14:50:24 +0200

    Department registrations

--- client/app/routing.module.ts
+++ client/app/routing.module.ts
@@ -8,6 +8,7 @@ import { LoginComponent } from './login/login.component';
 import { LogoutComponent } from './logout/logout.component';
 import { AccountComponent } from './account/account.component';
 import { AdminComponent } from './admin/admin.component';
+import { DepartmentsComponent } from './departments/departments.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 import { AuthGuardLogin } from './services/auth-guard-login.service';
@@ -17,6 +18,7 @@ const routes: Routes = [
   { path: '', component: AboutComponent },
   { path: 'cats', component: CatsComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'department', component: DepartmentsComponent },
   { path: 'login', component: LoginComponent },
   { path: 'logout', component: LogoutComponent },
   { path: 'account', component: AccountComponent, canActivate: [AuthGuardLogin] },
